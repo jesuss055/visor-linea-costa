f2caa27696b2cd3e472c01b558427120e9db9bee
jesuss055 <jesuss.tapia.jtc@gmail.com>
2025-07-10 01:21:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 var map = L.map('map').setView([18.43, -93.3], 9);
 var imageLayer, vectorLayer;
 var imageBounds = [[18.20, -94.131], [18.66, -92.465]];
+var vectorLayerComparar; // Para la segunda línea (después)
+
 
 // Capa base (opcional)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -15,12 +17,20 @@ var years = [1986, 1993, 1997, 2004, 2007, 2011, 2016, 2021, 2025];
 
 // Inicializar selectores
 var yearSelector = document.getElementById('yearSelector');
+var yearComparar = document.getElementById('yearComparar');
+
 years.forEach(function(year) {
-  var option = document.createElement('option');
-  option.value = year;
-  option.text = year;
-  yearSelector.appendChild(option);
+  let option1 = document.createElement('option');
+  option1.value = year;
+  option1.text = year;
+  yearSelector.appendChild(option1);
+
+  let option2 = document.createElement('option');
+  option2.value = year;
+  option2.text = year;
+  yearComparar.appendChild(option2.cloneNode(true));
 });
+
 
 // Función para cargar imagen y vector
 function loadLayers(year) {
@@ -30,11 +40,15 @@ function loadLayers(year) {
 
   // Cargar imagen satelital RGB
   imageLayer = L.imageOverlay('images/RGB_' + year + '.jpg', imageBounds);
-  imageLayer.addTo(map);
+  
+  // Solo agregarla si el checkbox está activo
+  var checkbox = document.getElementById('toggleImage');
+  if (checkbox.checked) {
+    imageLayer.addTo(map);
+  }
 
   // Cargar línea de costa
   fetch('data/' + year + '.geojson')
-
     .then(res => res.json())
     .then(data => {
       vectorLayer = L.geoJSON(data, {
@@ -48,11 +62,59 @@ function loadLayers(year) {
       console.error('No se pudo cargar línea de costa para el año:', year);
     });
 }
+//Logica para comparar lineas de costa
+function cargarComparacion(year) {
+  if (vectorLayerComparar) {
+    map.removeLayer(vectorLayerComparar);
+  }
+
+  fetch('data/' + year + '.geojson')
+    .then(res => res.json())
+    .then(data => {
+      vectorLayerComparar = L.geoJSON(data, {
+        style: {
+          color: 'blue',
+          weight: 2,
+          dashArray: '5, 5'
+        }
+      }).addTo(map);
+    })
+    .catch(err => {
+      console.error('No se pudo cargar línea de costa (DESPUÉS):', year);
+    });
+}
+
 
 // Listener para cambios de año
 yearSelector.addEventListener('change', function() {
+//Verificar check box para mostrar imagen
+document.getElementById('toggleImage').addEventListener('change', function () {
+  if (this.checked) {
+    if (imageLayer) imageLayer.addTo(map);
+  } else {
+    if (imageLayer) map.removeLayer(imageLayer);
+  }
+});
   var selectedYear = this.value;
   loadLayers(selectedYear);
+});
+// comparacion de linea de costa check box
+document.getElementById('toggleComparar').addEventListener('change', function () {
+  var year = yearComparar.value;
+  if (this.checked && year !== '') {
+    cargarComparacion(year);
+  } else {
+    if (vectorLayerComparar) {
+      map.removeLayer(vectorLayerComparar);
+    }
+  }
+});
+
+yearComparar.addEventListener('change', function () {
+  var check = document.getElementById('toggleComparar');
+  if (check.checked) {
+    cargarComparacion(this.value);
+  }
 });
 
 // Carga inicial
